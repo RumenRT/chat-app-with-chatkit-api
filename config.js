4718d0fa38f62f73eef608f5ad9dcb1673db4dc7
RumenRT <33309rt@gmail.com>
2018-11-05 21:08:45 -0500

    final

---
+++ config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/cc8a2a20-d6fe-4fe2-beaf-b6c55dac50ce/token";
+const instanceLocator = "v1:us1:cc8a2a20-d6fe-4fe2-beaf-b6c55dac50ce";
+// const roomId = 19373079
+// const username = 'rumbo'
+export { tokenUrl, instanceLocator }
